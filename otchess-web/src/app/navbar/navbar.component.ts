f0b7ba1223290838ea9803c88621f4fdbf9a0767
Piotr Nowak <npiotrek44@gmail.com>
2026-01-11 22:22:14 +0100

    feat: Lower material components density

--- otchess-web/src/app/navbar/navbar.component.ts
+++ otchess-web/src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatToolbar],
+  imports: [MatToolbar, MatButton],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
