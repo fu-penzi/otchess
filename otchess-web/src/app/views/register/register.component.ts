f5e1e42c4f4bedb873e176863cc99e1b45c11f93
Piotr Nowak <npiotrek44@gmail.com>
2026-01-21 19:06:39 +0100

    feat: Register page

--- otchess-web/src/app/views/register/register.component.ts
+++ otchess-web/src/app/views/register/register.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatDivider } from '@angular/material/list';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UrlEnum } from '@app/app.routes';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatDivider,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  private _fb = inject(FormBuilder);
+  private _router = inject(Router);
+
+  ngOnInit(): void {
+    this.form = this._fb.group({
+      login: '',
+      email: '',
+      password: '',
+    });
+  }
+
+  signUp(): void {
+    console.log(this.form.value);
+  }
+
+  goToLogin(): void {
+    this._router.navigateByUrl(UrlEnum.LOGIN);
+  }
+}
